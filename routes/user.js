d4577b6ef84370f11916678541e8f4a2e13c46e5
Suhas Saheer <s.saheer@adfgs-MacBook-Pro-2.local>
2019-07-18 11:30:38 +0400

    Update & Delete User REST API added + swagger documentation

--- routes/user.js
+++ routes/user.js
@@ -11,9 +11,9 @@ router.post('/create', user_controller.user_create);
 
 router.get('/:id', user_controller.user_details);
 
-router.put('/:id/update', user_controller.user_update);
+router.put('/:id', user_controller.user_update);
 
-router.delete('/:id/delete', user_controller.user_delete);
+router.delete('/:id', user_controller.user_delete);
 
 router.get('/', user_controller.user_findAll);
 
